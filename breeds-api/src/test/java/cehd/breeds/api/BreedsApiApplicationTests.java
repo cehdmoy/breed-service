ed2879abd9071bce020186760cfc0170b3883da3
Roberto Moya <robertomoyasuarez@gmail.com>
2020-04-27 19:27:57 -0400

    Primer intento de automatizar deploy local

--- breeds-api/src/test/java/cehd/breeds/api/BreedsApiApplicationTests.java
+++ breeds-api/src/test/java/cehd/breeds/api/BreedsApiApplicationTests.java
@@ -2,8 +2,11 @@ package cehd.breeds.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("dev")
 class BreedsApiApplicationTests {
 
 	@Test
---
+++ config-service/compile.sh
@@ -0,0 +1,2 @@
+mvn clean package
+java -jar ./target/config-service-0.0.1-SNAPSHOT.jar
